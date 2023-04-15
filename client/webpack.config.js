84eaad39578969974cdc8f827f765d0365a9cb88
alex.kim <alkkim@gmail.com>
2023-04-15 13:52:11 -0700

    added css and babel rules

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -52,7 +52,23 @@ module.exports = () => {
 
     module: {
       rules: [
-
+        // CSS loaders
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
+        },
+        // Add babel webpack
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime",],
+            },
+          },
+        },
       ],
     },
   };
