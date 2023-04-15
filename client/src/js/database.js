6162e2d82202c48d853af23b1088082f47d4c190
alex.kim <alkkim@gmail.com>
2023-04-15 14:54:28 -0700

    added scripts to package.json file

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -24,7 +24,7 @@ export const putDb = async (content) => {
   const request = store.put({ jate: content });
   // confirms the status and console.logs the result
   const result = await request;
-  console.log("Data saved to the database", result);
+  console.log("Data saved", result);
 };
 
 // Add logic for a method that gets all the content from the database
